d0c563a8013b4101cd3718c586ef0283fae4275c
Anthony Kawa <anthonyk2020@gmail.com>
2020-03-10 14:28:17 -0700

    MVP goals

--- react-github-user-card/src/components/User.js
+++ react-github-user-card/src/components/User.js
@@ -1,22 +1,15 @@
 import React from "react";
 import UserDetails from "./UserDetails";
-import { makeStyles } from "@material-ui/core/styles";
 import styled from 'styled-components';
-import clsx from "clsx";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import Collapse from "@material-ui/core/Collapse";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 
 const Root = styled(Card)`
@@ -34,38 +27,15 @@ const Expand = styled(ExpandMoreIcon)`
   transform: ${props => props.expanded ? 'rotate(180deg)' : 'rotate(0deg);'}
   
 `;
-const useStyles = makeStyles(theme => ({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 0,
-    paddingTop: "56.25%" // 16:9
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest
-    })
-  },
-  expandOpen: {
-    transform: "rotate(180deg)"
-  },
-  avatar: {
-    backgroundColor: red[500]
-  }
-}));
+
+const AvatarStyle = styled(Avatar)`
+  background-color: ${red[500]} !important;
+`;
 
 export default class User extends React.Component {
   state = {
     expanded: false
   };
-
-  classes = () => {
-    const classes = useStyles();
-    return classes;
-  }
 
   handleExpandClick = () => {
     this.setState(prevState => ({
@@ -77,16 +47,13 @@ export default class User extends React.Component {
       <Root>
         <CardHeader
           avatar={
-            <Avatar aria-label={this.props.user.name} className={this.classes.avatar} >A</Avatar>
+            <AvatarStyle aria-label={this.props.user.name} >A</AvatarStyle>
           }
 
           title={this.props.user.name}
         />
         <Media image={this.props.user.avatar_url} title={this.props.user.name} />
         <IconButton
-          className={clsx(this.classes.expand, {
-            [this.classes.expandOpen]: this.state.expanded,
-          })}
           onClick={this.handleExpandClick}
           aria-expanded={this.state.expanded}
           aria-label="show more"
